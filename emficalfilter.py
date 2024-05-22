42234b9556307243c746f6cc910f43d6d55a9250
Johnr24 <johnrogers24@googlemail.com>
2024-05-22 22:45:26 +0100

    split up the main cal to 4 others using last years

--- emficalfilter.py
+++ emficalfilter.py
@@ -1,10 +1,11 @@
 from icalendar import Calendar
 import requests
-#
+
+year = 24
 # Download the 2024.ical file from the given URL
-url = 'https://www.emfcamp.org/schedule/2024.ical'
+url = 'https://www.emfcamp.org/schedule/2022.ical'
 response = requests.get(url)
-with open('2024.ical', 'wb') as file:
+with open('./2024.ical', 'wb') as file:
     file.write(response.content)
 
 # Open the existing .ical file and parse it into a Calendar object
@@ -20,10 +21,16 @@ for event in cal.walk('vevent'):
     # If the event's location contains "stage", add it to the filtered calendar
     if 'stage' in event.get('location', '').lower():
         stage_cal.add_component(event)
+for event in cal.walk('vevent'):
     if 'workshop' in event.get('location', '').lower():
-        stage_cal.add_component(event)
+        workshop_cal.add_component(event)
+for event in cal.walk('vevent'):
+    if 'sector' in event.get('location', '').lower():
+        nullsector_cal.add_component(event)
 # Write the filtered calendar to a new .ical file
 with open('stage.ical', 'wb') as file:
     file.write(stage_cal.to_ical())
 with open('workshop.ical', 'wb') as file:
     file.write(workshop_cal.to_ical())
+with open('nullsector.ical', 'wb') as file:
+    file.write(nullsector_cal.to_ical())
